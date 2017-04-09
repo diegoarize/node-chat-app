83c9772d2004d68d3b476eaafce8af666bbaf91c
diegoarize <diegoarize@outlook.com>
2017-04-09 11:06:40 -0300

    Add connect and disconnect events

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Node Chat App using socket.io
 
 ## under development (for learning purposes)
+
+- Heroku app
+https://node-socket-io-chat-app.herokuapp.com/
--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+
+var io = socketIO(server);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('New User connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client Disconnected');
+  });
+});
+
+
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
