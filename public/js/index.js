c40ff331399922065e3c819974bc74f27839ebe3
diegoarize <diegoarize@outlook.com>
2017-04-17 19:45:48 -0300

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -11,22 +11,27 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
   var formattedTime = moment(message.createdAt).format('h:mm a')
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  $('#messages').append(li);
+  $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', (message) => {
   var formattedTime = moment(message.createdAt).format('h:mm a')
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My current location</a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url)
 
-  li.append(a);
+  var template = $('#location-message-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  $('#messages').append(li);
+  $('#messages').append(html);
 });
 
 
